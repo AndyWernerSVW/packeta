78633c3b92579c3b588b9e40d6d962c25e412a68
derzeitgeist.eth <derzeitgeist99@icloud.com>
2024-06-27 12:56:23 +0200

    Scraper overhaul:

--- main.py
+++ main.py
@@ -2,33 +2,45 @@ import streamlit as st
 import Helpers.margin_pool as mp
 import Helpers.depot_utilisation as dp
 import Helpers.key_values as kv
-import Helpers.scraper_gemini as sg
+import Helpers.scraper_main as sm
 import pandas as pd
 
 
-# Display Margin Pool chart
 st.title('Great customer A')
-st.subheader("Margin Pool")
+tab1, tab2 = st.tabs(["Scraper","Dashboard"])
+with tab1:
+    st.header('Delivery option Scraper and Analyzer')
+    with st.expander("Worth trying"):
+        st.code("""https://bionebio.cz/doprava-zbozi
+    https://www.datart.cz/napoveda/vsechny-druhy-doprav
+    https://gymbeam.cz/doruceni-platba
+    https://www.mall.cz/zpusoby-doruceni
+        """)
 
-margin_pool_data  = pd.read_csv('SampleData/inputs.csv')
+    section1 = st.empty()
+    sm.scraper_main(section1)
 
-st.pyplot(mp.margin_pool_chart(margin_pool_data))
-with st.expander("Notes"):
-    st.markdown(mp.mp_notes)
 
-st.subheader("Depot Utilization")
-depot_names = [
-    "Nučice u Rudné u Prahy", "Praha-Štěrboholy", "Nehvizdy u Prahy",
-    "Holubice u Brna", "Ostrava", "Ústí nad Labem", "Břeclav",
-    "Plzeň", "České Budějovice 'Minidepo'", "Hradec Králové", "Zlín",
-    "Jihlava", "Olomouc", "Šumperk", "Liberec", "Karlovy Vary 'Minidepo'", "Svitavy"]
-depot_util = dp.plot_depot_utilization(depot_names)
-st.pyplot(depot_util)
-with st.expander("Notes"):
-    st.markdown(dp.data_needed)
+with tab2:
+    # Display Margin Pool chart
+    st.subheader("Margin Pool")
 
-st.subheader("Key Values and Benchmarking")
-kv.key_ratios()
+    margin_pool_data  = pd.read_csv('SampleData/inputs.csv')
 
-st.header('Delivery option Scraper and Analyzer')
-sg.scraper()
+    st.pyplot(mp.margin_pool_chart(margin_pool_data))
+    with st.expander("Notes"):
+        st.markdown(mp.mp_notes)
+
+    st.subheader("Depot Utilization")
+    depot_names = [
+        "Nučice u Rudné u Prahy", "Praha-Štěrboholy", "Nehvizdy u Prahy",
+        "Holubice u Brna", "Ostrava", "Ústí nad Labem", "Břeclav",
+        "Plzeň", "České Budějovice 'Minidepo'", "Hradec Králové", "Zlín",
+        "Jihlava", "Olomouc", "Šumperk", "Liberec", "Karlovy Vary 'Minidepo'", "Svitavy"]
+    depot_util = dp.plot_depot_utilization(depot_names)
+    st.pyplot(depot_util)
+    with st.expander("Notes"):
+        st.markdown(dp.data_needed)
+
+    st.subheader("Key Values and Benchmarking")
+    kv.key_ratios()
